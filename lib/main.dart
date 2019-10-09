bf5bed21351ea46778b7d2cd49bdd306ac6cbd22
ahmed <sci.ahmedeldamarany2016@alexu.edu.eg>
2019-10-09 22:49:26 +0200

    containerColorChanged

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       darkTheme: ThemeData.dark(),
       title: 'KIK',
-      initialRoute: '/second',
+      initialRoute: '/',
       routes: {
         '/': (context)=> LoginScreen(),
         '/first': (context)=> FriendsList(),
