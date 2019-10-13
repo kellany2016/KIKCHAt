8cd3888361381a27d02a8f9e8e9fe148708c0825
Ahmed Muhammed Alaa <ahmedkellany51@gmail.com>
2019-10-13 20:49:04 +0200

    Hello to KIK chat

--- lib/screens/Photographia.dart
+++ lib/screens/Photographia.dart
@@ -6,7 +6,7 @@ import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:async';
 import 'loginScreen.dart';
-
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 class Photographia extends StatefulWidget {
   @override
