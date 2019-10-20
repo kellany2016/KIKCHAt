bc4acac7f62841f4dca5bce84727de1219c26592
ahmed <sci.ahmedeldamarany2016@alexu.edu.eg>
2019-10-20 06:34:54 +0200

    fixup! All designed except for photographia

--- lib/screens/friendsList.dart
+++ lib/screens/friendsList.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:kik_chat/auth.dart';
+import 'package:kik_chat/constants.dart';
+
 import 'root_page.dart';
 
 class FriendsList extends StatefulWidget {
   final BaseAuth auth;
   final VoidCallback signedOut;
-  FriendsList({this.auth,this.signedOut});
+  FriendsList({this.auth, this.signedOut});
   @override
   _FriendsListState createState() => _FriendsListState();
 }
 
 class _FriendsListState extends State<FriendsList> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: KmyColors[3],
           title: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text('KIK'),
@@ -30,7 +32,11 @@ class _FriendsListState extends State<FriendsList> {
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
+          backgroundColor: KmyColors[2],
+          child: Icon(
+            Icons.add,
+            size: 30,
+          ),
           onPressed: null, // add user by name or phone..
         ),
         body: Column(
@@ -39,7 +45,13 @@ class _FriendsListState extends State<FriendsList> {
             Expanded(
               child: ListView(
                 children: <Widget>[
-                  friendBubble(),
+                  friendBubble(
+                      name: 'Eslam Sannoofaa',
+                      lastMsg: 'ممكن نتعرف ؟',
+                      lastTime: 12.12,
+                      pfp: AssetImage(
+                        'assets/images/me.jpg',
+                      )),
                 ],
               ),
             ),
@@ -49,56 +61,63 @@ class _FriendsListState extends State<FriendsList> {
     );
   }
 
-  Widget friendBubble() {
+  Widget friendBubble(
+      {String name, String lastMsg, double lastTime, ImageProvider pfp}) {
     return Card(
-      child: InkWell(
-        onTap: () => null,
-        child: Container(
-          height: 150.0,
-          width: 100.0,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/ic_launcher.png',
-                      height: 40.0,
-                      width: 40.0,
-                    ), //friend image url ..
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 4.0, 0.0, 0.0),
-                      child: Text(
-                        'friend name',
-                        style: TextStyle(fontSize: 40.0),
-                      ),
-                    ),
-                  ],
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: CircleAvatar(radius: 30, backgroundImage: pfp
+                //Todo u can use NetworkImage ()
                 ),
-
-              ),
-            ],
           ),
-        ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  name,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: KmyColors[0]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    lastMsg,
+                    style: TextStyle(fontSize: 16, color: KmyColors[1]),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Text(
+            '$lastTime',
+            style: TextStyle(color: KmyColors[1]),
+          ),
+        ],
       ),
     );
   }
 
-  String searchUserName(String username){
+  String searchUserName(String username) {
     //search user names in the list..
     return null;
   }
+
   //handle sign out and go back to root page, root page should go to login screen..
-  void _signOut()async{
-    try{
+  void _signOut() async {
+    try {
       await widget.auth.signOut();
       widget.signedOut();
-      Navigator.push(
-          context , MaterialPageRoute(builder: (context) => RootPage(auth: widget.auth)));
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => RootPage(auth: widget.auth)));
+    } catch (e) {
+      print(e);
     }
-    catch(e){print(e);}
   }
 }
-
